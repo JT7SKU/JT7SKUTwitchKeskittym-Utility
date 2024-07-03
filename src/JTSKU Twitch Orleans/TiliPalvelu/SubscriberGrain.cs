e0c1a44897a4fc6e814426636910cf3445598b4b
Janisku7 <janisku7@gmail.com>
2024-07-03 23:18:41 +0300

    updates

---
+++ src/JTSKU Twitch Orleans/TiliPalvelu/SubscriberGrain.cs
@@ -0,0 +1,22 @@
+﻿using Orleans;
+using Services.Kontrakti.Unit.Twitch.Tili;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Services.Kirjasto.Unit.Twitch.Tili
+{
+    public class SubscriberGrain : Grain, ITwitchSubscriber
+    {
+        public override async Task OnActivateAsync(CancellationToken cancellationToken)
+        {
+            await Task.CompletedTask;
+        }
+        public Task NewSubscriber(IUser user,string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/Services/StreamKomponentsUnit.Twitch.Services/obj/Debug/net8.0/StreamKomponentsUnit.Twitch.Services.AssemblyInfo.cs
+++ src/Services/StreamKomponentsUnit.Twitch.Services/obj/Debug/net8.0/StreamKomponentsUnit.Twitch.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StreamKomponentsUnit.Twitch.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18868abd2eaf530812ececa3905c088842c8ae7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfbc709900889f9b2556156263749e47b54e8538")]
 [assembly: System.Reflection.AssemblyProductAttribute("StreamKomponentsUnit.Twitch.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StreamKomponentsUnit.Twitch.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
